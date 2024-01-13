fc580bf7e9a260a83ded28086bae5fdd5e5e2997
u199143 <robert.cordova@l3harris.com>
2024-01-13 13:20:41 -0500

    Change Theme, add Clock, handle errors

--- gui.py
+++ gui.py
@@ -1,6 +1,9 @@
 import todo_functions
 import PySimpleGUI as sg
+import time
 
+sg.theme('DarkAmber')
+clock = sg.Text('',key='Clock')
 label = sg.Text('Type in a todo')
 input_box = sg.InputText(tooltip='Enter a todo',key = 'todo_input')
 add_button = sg.Button('Add')
@@ -12,16 +15,15 @@ edit_button = sg.Button('Edit')
 complete_button = sg.Button('Complete')
 exit_button = sg.Button('Exit')
 window = sg.Window('My To-Do App',
-                   layout=[[label],
+                   layout=[[clock],
+                           [label],
                            [input_box, add_button],
                            [list_box,edit_button,complete_button],
                            [exit_button]],
                    font=('Helvetica', 20))
 while True:
-    event, values = window.read()
-    print(1,event)
-    print(2,values)
-    print(3,values['todos_list'])
+    event, values = window.read(timeout=200)
+    window['Clock'].update(value= time.strftime('%b %d, %Y %H:%M'))
     match event:
         case 'Add':
             todos = todo_functions.get_todos()
@@ -30,16 +32,23 @@ while True:
 
             window['todos_list'].update(values=todos)
         case 'Edit':
-            old_todo = values['todos_list'][0]
+            try:
+                old_todo = values['todos_list'][0]
+            except IndexError:
+                sg.popup('Please select an item first',font=('Helvetica',20))
+                continue
             new_todo = values['todo_input']
-
             todos = todo_functions.get_todos()
             index = todos.index(old_todo)
             todos[index] = new_todo
             todo_functions.write_todos(todos)
             window['todos_list'].update(values = todos)
         case 'Complete':
-            todo_to_complete = values['todos_list'][0]
+            try:
+                todo_to_complete = values['todos_list'][0]
+            except IndexError:
+                sg.popup('Please select an item first',font=('Helvetica',24))
+                continue
             todos = todo_functions.get_todos()
             todos.remove(todo_to_complete)
             todo_functions.write_todos(todos)
